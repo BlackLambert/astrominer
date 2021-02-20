5c4d16aa7dc45fe40d303941e67df5034aff3456
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2021-02-20 20:14:23 +0100

    Removes test scene. Changes the FindSingleInstance function to a more performant solution.

--- astrominerProject/Assets/Scripts/AppScripts/Utility/SceneObjectFinder.cs
+++ astrominerProject/Assets/Scripts/AppScripts/Utility/SceneObjectFinder.cs
@@ -1,24 +1,37 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Astrominer
 {
     public class SceneObjectFinder
     {
-        public static T FindSingleInstance<T>()
+		// Source: https://answers.unity.com/questions/863509/how-can-i-find-all-objects-that-have-a-script-that.html
+		public static T FindSingleInstance<T>()
 		{
-            IEnumerable<T> searchResult = GameObject.FindObjectsOfType<MonoBehaviour>().OfType<T>();
-            if (searchResult.Count() == 0)
-                throw new NoInstanceFoundException();
-            if (searchResult.Count() > 1)
-                throw new TooManyInstancesExceptions();
-            return searchResult.First();
-        }
+			IEnumerable<T> searchResult = GetInstancesOf<T>();
+			if (searchResult.Count() == 0)
+				throw new NoInstanceFoundException();
+			if (searchResult.Count() > 1)
+				throw new TooManyInstancesExceptions();
+			return searchResult.First();
 
-        public class NoInstanceFoundException : Exception { }
+			static IEnumerable<T> GetInstancesOf<T>()
+			{
+				List<T> result = new List<T>();
+				GameObject[] rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+				foreach (var rootGameObject in rootGameObjects)
+				{
+					T[] childrenInterfaces = rootGameObject.GetComponentsInChildren<T>();
+					result.AddRange(childrenInterfaces);
+				}
+				return result;
+			}
+		}
+
+		public class NoInstanceFoundException : Exception { }
         public class TooManyInstancesExceptions : Exception { }
     }
 }
